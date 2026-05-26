50f24da3c48d159af93af244ae2823adf8cc08e6
wonjiee <wonjiee01@gmail.com>
2026-05-26 17:04:43 +0900

    백엔드 기본 CRUD 구현

---
+++ backend/src/main/java/com/jiwon/portfolio/backend/controller/ProjectController.java
@@ -0,0 +1,50 @@
+package com.jiwon.portfolio.backend.controller;
+
+import com.jiwon.portfolio.backend.entity.Project;
+import com.jiwon.portfolio.backend.repository.ProjectRepository;
+
+import com.jiwon.portfolio.backend.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private final ProjectService service;
+
+    @GetMapping
+    public List<Project> getProjects(){
+
+        return service.getProjects();
+    }
+
+    @PostMapping
+    public Project createProject(
+            @RequestBody Project project){
+
+        return service.createProject(project);
+    }
+
+    @PutMapping("/{id}")
+    public Project updateProject(
+            @PathVariable Long id,
+            @RequestBody Project project){
+
+        return service.updateProject(
+                id,
+                project);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProject(
+            @PathVariable Long id){
+
+        service.deleteProject(id);
+    }
+
+}
